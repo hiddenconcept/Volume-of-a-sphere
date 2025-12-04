c8f49bd22330c9a647a2b5b868c6544888a6a349
hiddenconcept <myles.patricio@gmail.com>
2025-12-04 01:08:15 -0500

    Add files via upload

---
+++ Calculate Volume Of Sphere(15).py
@@ -0,0 +1,6 @@
+#The volume of the sphere is : V = 4/3 × π × r3 = π × d3/6.
+
+pi = 3.1415926535
+radius = float(input("Enter the radius of the sphere: "))
+volume = 4/3 * pi * radius ** 3
+print(f"The volume of the sphere is: ",volume)
